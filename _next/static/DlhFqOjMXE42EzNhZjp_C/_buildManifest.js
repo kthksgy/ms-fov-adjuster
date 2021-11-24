bd38c216cce0b687c8a3a241cb0b6dee407369b8
kthksgy <kthksgy@users.noreply.github.com>
2021-11-24 12:17:29 +0000

    deploy: c5e0a703b190422cbb59dc8443c7baaaa10280b6

---
+++ _next/static/DlhFqOjMXE42EzNhZjp_C/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/587-3894b95b0132faf9.js","static/chunks/pages/index-3f2cb9a5ee791850.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
