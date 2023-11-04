6d4f1347e292ced6d3dd8a51621d3b608df7cc43
kthksgy <1616191+kthksgy@users.noreply.github.com>
2023-11-04 11:08:30 +0900

    Next.jsからViteに移行するため新規作成

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '';
-
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix: urlPrefix,
-  basePath: urlPrefix,
-  trailingSlash: true,
-}
